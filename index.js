4a11df801045f5d00603d31830641eddce8afe3a
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-10-19 17:38:34 +0100

    adds database set up

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 const restaurantsRoutes = require('./routes/restaurants');
+
+mongoose.connect('mongodb://localhost/restaurants')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(() => console.log('Connection to MongoDB failed...'));
 
 app.use('/api/restaurants', restaurantsRoutes);
 
