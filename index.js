ed0c7e0164fdf84c61f9e0e70c19dfe4be94360a
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-10-20 12:32:35 +0100

    adds cors refactoring

--- index.js
+++ index.js
@@ -10,24 +10,13 @@ mongoose.connect('mongodb://testuser:testuser1@ds016138.mlab.com:16138/restauran
 
 app.use(express.json());
 app.use(function (req, res, next) {
-  // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Origin', 'https://web-ui-service.herokuapp.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
   next();
 });
-
-
 
 app.use('/api/restaurants', restaurantsRoutes);
 
