9998da9610c2b30a7cb01c660c42b9de873a8e00
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-10-24 15:11:03 +0100

    updates asyncMiddleware to be used by all routes

--- src/routes/restaurants.js
+++ src/routes/restaurants.js
@@ -1,27 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const model = require('../models/restaurants');
+const asyncMiddleware = require('../middleware/async');
 
-router.get('/', async (req, res, next) => {
-  try {
+router.get('/', asyncMiddleware(async (req, res) => {
     const restaurants = await model.getRestaurants();
     res.send(restaurants);
-  } catch (error) {
-    next(error);
-  }
-});
+}));
 
-router.get('/:postcode', async (req, res) => {
+router.get('/:postcode', asyncMiddleware(async (req, res) => {
   const restaurant = await model.getRestaurant(req.params);
-  if (!restaurant) {
-    return res.status(404).send('There are no restaurants in that postcode!');
-  }
   res.send(restaurant);
-});
+}));
 
-router.post('/', async (req, res) => {
+router.post('/', asyncMiddleware(async (req, res) => {
   const restaurant = await model.postRestaurant(req.body);
   res.send(restaurant);
-});
+}));
 
 module.exports = router;
