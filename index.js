f10d7e525dd4a99ac406c93575d44ebeb9bb4a9b
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-03-14 11:45:53 +0000

    fixes nginx bad gateway error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const error = require('./src/middleware/error');
 const corsMiddleware = require('./src/middleware/cors');
 const restaurantsRoutes = require('./src/routes/restaurants');
 const port = process.env.PORT || 3001;
@@ -8,7 +7,6 @@ const port = process.env.PORT || 3001;
 require('./src/startup/db')();
 
 app.use(corsMiddleware);
-app.use(error);
 
 app.use(express.json());
 app.use('/api/restaurants', restaurantsRoutes);
